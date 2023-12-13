a40aa86cf4aa56fe970648c355e49ffa753a880e
ZIHCO <chizobajames21@gmail.com>
2023-12-13 16:06:18 +0100

    asyncio sort list

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,9 +3,12 @@
 using concurrency"""
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
+from typing import List
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """return sorted list using asyncio"""
-    routines = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    return [await routine for routine in asyncio.as_completed(routines)]
+    routines = [asyncio.create_task(wait_random(max_delay))
+                for _ in range(n)]
+    return [await routine
+            for routine in asyncio.as_completed(routines)]
