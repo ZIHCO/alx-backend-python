0a1f1013700464f3a9c85d10d774282fd41a6ff3
ZIHCO <chizobajames21@gmail.com>
2024-01-12 18:44:04 +0100

    basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This script contains a type-annotated function add."""
+
+
+def add(a: float, b: float) -> float:
+    """add performs additions on its args"""
+    return a + b
