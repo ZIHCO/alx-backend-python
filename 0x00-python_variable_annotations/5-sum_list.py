133a27236eda2666897beaf5c65d2948f06f8192
ZIHCO <chizobajames21@gmail.com>
2024-01-12 21:51:46 +0100

    complex types - list of float

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,7 +2,7 @@
 """script contains sum_list"""
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: [list[float]]) -> float:
     """compute the sum of the list arg"""
     sum = 0
     for number in input_list:
