367b775d832cc9417b0b05697a5e2ee54fe6303b
ZIHCO <chizobajames21@gmail.com>
2024-01-17 04:25:42 +0100

    async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""contains coroutine async_generator"""
+import random
+import asyncio
+
+
+async def async_generator():
+    """loops 10 time, each time waits a second
+    then yield a random number"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 1)
