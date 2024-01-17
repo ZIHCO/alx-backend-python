092583d9c6b7ed4f6208a8a7ba994130e5d56260
ZIHCO <chizobajames21@gmail.com>
2024-01-17 05:29:43 +0100

    sub Generator for Iterator

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """contain coroutine async_comprehension"""
 import asyncio
-from typing import Generator
+from typing import Generator, List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, list]:
+async def async_comprehension() -> Generator[float, None, List[float]]:
     """async comprehension"""
     return [i async for i in async_generator()]
