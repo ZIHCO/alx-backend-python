ae9cd683e7cbdac0c9a55377e3a0d358c3a5e414
ZIHCO <chizobajames21@gmail.com>
2024-01-17 05:07:02 +0100

    type annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """contains coroutine async_generator"""
 import random
 import asyncio
+from typing import Iterator
 
 
-async def async_generator():
+async def async_generator() -> Iterator[float]:
     """loops 10 time, each time waits a second
     then yield a random number"""
     for i in range(10):
